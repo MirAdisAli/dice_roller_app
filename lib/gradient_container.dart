2a8116c95717239a64c1677be77131d865d9a717
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2024-02-11 00:37:19 +0600

    added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:dice_roller_app/styled_text.dart';
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 51, 27, 105),
-                Color.fromARGB(255, 70, 29, 128),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        );
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 51, 27, 105),
+            Color.fromARGB(255, 70, 29, 128),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
   }
 }
