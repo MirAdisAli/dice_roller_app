10599a0243701c10ea32345a9545445633c87a2b
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2024-02-11 23:10:47 +0600

    added

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 7, 5, 109),
+          Color.fromARGB(255, 62, 15, 138),
+        ),
       ),
     ),
   );
