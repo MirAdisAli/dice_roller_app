b65b2f29fa425736c2d427aa02f92f41f839d429
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2024-02-12 23:59:36 +0600

    added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -15,9 +15,7 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice() {
-
-  }
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -31,6 +29,7 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
@@ -38,6 +37,12 @@ class GradientContainer extends StatelessWidget {
             ),
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
               child: const Text('Roll Dice'),
             )
           ],
